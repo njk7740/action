9d842639ff4df161039f0bca4d2a0b05c00c92fb
njk7740 <njk7705@naver.com>
2023-12-11 19:09:58 +0900

    origin

---
+++ src/main/java/com/korea/basic1/MainController.java
@@ -0,0 +1,12 @@
+package com.korea.basic1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String index() {
+        return "main";
+    }
+}
